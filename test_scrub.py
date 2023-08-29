1c1a2dd0d0d859d6a25dde693f72f6e5ef95ae70
KALEY SULLIVAN <kaley.sullivan@gmail.com>
2023-08-29 10:55:58 -0500

    basic recursion set up

--- test_scrub.py
+++ test_scrub.py
@@ -8,7 +8,7 @@ from main import scrub
 
 @pytest.mark.parametrize("case_path", [f.path for f in os.scandir('./tdd_cases') if f.is_dir()])
 def test_tdd_case(case_path: str):
-    input = json.dumps(open(f"{case_path}/input.json", "r").readlines())
-    output = json.dumps(open(f"{case_path}/output.json", "r").readlines())
+    input = json.load(open(f"{case_path}/input.json"))
+    output = json.load(open(f"{case_path}/output.json"))
     sensitive_fields = open(f"{case_path}/sensitive_fields.txt", "r").readlines()
     assert scrub(input, sensitive_fields) == output
